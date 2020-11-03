e75c2f2630570fc0f7c70d1f2ce2238f0985f517
abdallah <abab231196@gmail.com>
2020-11-03 22:19:45 +0200

    3

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import logo from '../assets/images/logo-white.png';
 
 const Header = () => {
   return (
     <header className='header'>
       <div className='log-box'>
-        <img src='logo-white.png' alt='Logo' className='logo' />
+        <img src={logo} alt='Logo' height='35px' />
+      </div>
+
+      <div className='heading'>
+        <h1 className='primary'>
+          <span className='main'>Outdoors</span>
+          <span className='sub'>is where life happens</span>
+        </h1>
       </div>
     </header>
   );
